b49702c32c0276c88394fb345ab841aec697855d
Maria <mariaparraramos@yahoo.com>
2025-10-15 14:09:06 +0200

    latest

--- app/app.py
+++ app/app.py
@@ -1,1 +1,10 @@
-print("hola")
+import pandas as pd
+import requests
+import os
+from sqlalchemy import create_engine
+
+if __name__ == "__main__":
+    
+
+query = "SELECT * FROM cleaned_df"
+pd.read_sql(query, engine)
